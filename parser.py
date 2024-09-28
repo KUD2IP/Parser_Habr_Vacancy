d84f9b4e02a6bfd93758ff8b5f094728fb861b13
ivanushka05 <akkaunt9043@gmail.com>
2024-09-29 00:24:36 +0300

    edit

--- parser.py
+++ parser.py
@@ -36,42 +36,42 @@ def save_to_db(id, company, vacancies, salary_from, salary_to, city, url):
     conn.commit()
 
 
-# def parsing_habr(query):
-page = 1
-id = 1
-while True:
-    url = f"https://career.habr.com/vacancies?page={page}&q=java&type=all"
+def parsing_habr(query):
+    page = 1
+    id = 1
+    while True:
+        url = f"https://career.habr.com/vacancies?page={page}&q=java&type=all"
 
-    response = requests.get(url)
+        response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    if soup.find("div", {"class": "vacancy-card"}) is None:
-        break
+        if soup.find("div", {"class": "vacancy-card"}) is None:
+            break
 
-    for link in soup.find_all("div", {"class": "vacancy-card__inner"}):
-        company_name = link.find("a", {"class": "link-comp link-comp--appearance-dark"}).text
-        vacancy_name = link.find("a", {"class": "vacancy-card__title-link"}).text
-        salary = link.find("div", {"class": "basic-salary"}).text
-        salary_from = ""
-        salary_to = ""
-        city = link.find("div", {"class": "vacancy-card__meta"})
-        cities = ""
-        for i in city.find_all("a", {"class": "link-comp link-comp--appearance-dark"}):
-            cities += i.text + " "
-        if salary == "":
-            salary_to = "не указано"
-        elif "от" and "до" in salary:
-            str = salary.replace(" ", "").replace("от", "").replace("до", " ")[:-1]
-            s = str.split(" ")
-            salary_from = s[0]
-            salary_to = s[1]
-        elif "от" in salary and "до" not in salary:
-            str = salary.replace("от", "").replace(" ", "")[:-1]
-            salary_from = str
-        elif "до" in salary and "от" not in salary:
-            str = salary.replace("до", "").replace(" ", "")[:-1]
-            salary_to = str
-        save_to_db(id, company_name, vacancy_name, salary_from, salary_to, cities, url)
-        id += 1
-    page += 1
+        for link in soup.find_all("div", {"class": "vacancy-card__inner"}):
+            company_name = link.find("a", {"class": "link-comp link-comp--appearance-dark"}).text
+            vacancy_name = link.find("a", {"class": "vacancy-card__title-link"}).text
+            salary = link.find("div", {"class": "basic-salary"}).text
+            salary_from = ""
+            salary_to = ""
+            city = link.find("div", {"class": "vacancy-card__meta"})
+            cities = ""
+            for i in city.find_all("a", {"class": "link-comp link-comp--appearance-dark"}):
+                cities += i.text + " "
+            if salary == "":
+                salary_to = "не указано"
+            elif "от" and "до" in salary:
+                str = salary.replace(" ", "").replace("от", "").replace("до", " ")[:-1]
+                s = str.split(" ")
+                salary_from = s[0]
+                salary_to = s[1]
+            elif "от" in salary and "до" not in salary:
+                str = salary.replace("от", "").replace(" ", "")[:-1]
+                salary_from = str
+            elif "до" in salary and "от" not in salary:
+                str = salary.replace("до", "").replace(" ", "")[:-1]
+                salary_to = str
+            save_to_db(id, company_name, vacancy_name, salary_from, salary_to, cities, url)
+            id += 1
+        page += 1
